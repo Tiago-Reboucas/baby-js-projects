9771253fbf19fd84c88d0640c6e8beeac1a7d98f
Tiago-Reboucas <tiago.reboucas90@gmail.com>
2022-05-31 10:25:36 -0300

    general update

--- counter/scripts.js
+++ counter/scripts.js
@@ -1,35 +1,66 @@
-var currentNumberElement = document.getElementById('currentNumber');
-var currentNumberBG = currentNumberElement.style.background;
+var currentNumberElem = document.getElementById('currentNumber');
+var currentNumberBG = currentNumberElem.style.background;
+var startNumberElem = document.getElementById('startNumber');
+var minNumberElem = document.getElementById('minNumber');
+var maxNumberElem = document.getElementById('maxNumber');
+
+startNumberElem.value = 0;
+minNumberElem.value = -10;
+maxNumberElem.value = 10;
+
 var currentNumber = 0;
+var minNumber = minNumberElem.valueAsNumber;
+var maxNumber = maxNumberElem.valueAsNumber;
+
+startNumberElem.min = minNumber;
+startNumberElem.max = maxNumber;
+
 
 function decrement(){
-  if (currentNumber > -10){
-    currentNumber = currentNumber - 1;
-    currentNumberElement.innerHTML = currentNumber;
-  }
-  
+  updateMinmax();
 
+  if (currentNumber > minNumber){
+    currentNumber = currentNumber - 1; 
+  }
   update();
 }
 
 function increment(){
-  if (currentNumber < 10){
-    currentNumber = currentNumber + 1;
-    currentNumberElement.innerHTML = currentNumber;
-  }
-  
+  updateMinmax();
 
+  if (currentNumber < maxNumber){
+    currentNumber = currentNumber + 1;
+  }
   update();
 }
 
 function update(){
   if (currentNumber == 0){
-    currentNumberElement.style.background = currentNumberBG;
+    currentNumberElem.style.background = currentNumberBG;
   }
   else if (currentNumber < 0){
-    currentNumberElement.style.background = 'red';
+    currentNumberElem.style.background = 'red';
   }
   else {
-    currentNumberElement.style.background = 'green';
+    currentNumberElem.style.background = 'green';
   }
+  currentNumberElem.innerHTML = currentNumber;
+}
+
+function reset(){
+  updateMinmax();
+
+  startNumber = startNumberElem.valueAsNumber;
+  if(startNumber > maxNumber){currentNumber = maxNumber}
+  else if(startNumber < minNumber){currentNumber = minNumber}
+  else{currentNumber = startNumber;}
+  
+  update();
+}
+
+function updateMinmax(){
+  minNumber = minNumberElem.valueAsNumber;
+  startNumberElem.min = minNumber;
+  maxNumber = maxNumberElem.valueAsNumber;
+  startNumberElem.max = maxNumber;
 }
